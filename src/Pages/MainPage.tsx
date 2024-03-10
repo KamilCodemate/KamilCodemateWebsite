a7b00a67f7554b8aea14acedbc54f25b4532f26f
Kamil Marczak <kamil.codemate@gmail.com>
2024-03-10 20:34:51 +0100

    Created basic navbar layout

---
+++ src/Pages/MainPage.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './PageStyles/MainPage.scss';
+const MainPage: React.FC = () => {
+  return (
+    <div className='container'>
+      <div className='main-navbar'>
+        <div className='navbar-logo'>InteFuture</div>
+        <div className='navbar-links'>
+          <div className='navbar-link'>HOME</div>
+          <div className='navbar-link'>Nasze usługi</div>
+          <div className='navbar-link'>Jak to działa</div>
+          <div className='navbar-link'>Kontakt</div>
+        </div>
+      </div>
+      <div className='hero'>
+        <div id='hero-left-col'></div>
+        <div id='hero-right-col'></div>
+      </div>
+    </div>
+  );
+};
+
+export default MainPage;
