9f094de4166259eee5b19e357fe9e9913c8f978f
Kamil Marczak <kamil.codemate@gmail.com>
2024-03-11 17:26:44 +0100

    Make navbar as separated component

---
+++ src/Components/MainNavbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './ComponentStyles/MainNavbar.scss';
+const MainNavbar: React.FC = () => {
+  return (
+    <div className='main-navbar'>
+      <div className='navbar-logo'>InteFuture</div>
+      <div className='navbar-links'>
+        <div className='navbar-link'>HOME</div>
+        <div className='navbar-link'>Nasze usługi</div>
+        <div className='navbar-link'>Jak to działa</div>
+        <div className='navbar-link'>Kontakt</div>
+      </div>
+    </div>
+  );
+};
+
+export default MainNavbar;
--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -2,18 +2,11 @@ import React from 'react';
 import './PageStyles/MainPage.scss';
 import LaptopImage from '../images/hero-laptop.jpg';
 import { MdWeb } from 'react-icons/md';
+import MainNavbar from '../Components/MainNavbar';
 const MainPage: React.FC = () => {
   return (
     <div className='container'>
-      <div className='main-navbar'>
-        <div className='navbar-logo'>InteFuture</div>
-        <div className='navbar-links'>
-          <div className='navbar-link'>HOME</div>
-          <div className='navbar-link'>Nasze usługi</div>
-          <div className='navbar-link'>Jak to działa</div>
-          <div className='navbar-link'>Kontakt</div>
-        </div>
-      </div>
+      <MainNavbar />
       <div className='hero'>
         <div id='hero-left-col'>
           <div id='hero-left-col-main-content'>
@@ -26,6 +19,7 @@ const MainPage: React.FC = () => {
         <div id='hero-right-col'>
           <img src={LaptopImage} alt='laptop' />
         </div>
+        M
       </div>
       <div id='services'>
         <div id='services-title'>
