83879e50affa75509f5d0b8ef7ad7195fc682416
Kamil Marczak <kamil.codemate@gmail.com>
2024-04-07 14:13:48 +0200

    Make footer as separated component

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './ComponentStyles/Footer.scss';
+const Footer: React.FC = () => {
+  return (
+    <div className='footer'>
+      <div className='footer-first-col'>
+        <div className='footer-logo'>InteFuture</div>
+        <div className='slogan'>Przyciągniemy do Ciebie uwagę</div>
+        <div className='footer-services-root'>
+          <div className='footer-services-title'>Usługi</div>
+          <div className='footer-services'>
+            <div className='footer-service'>Tworzenie witryn</div>
+            <div className='footer-service'>Aktualizacja witryn</div>
+            <div className='footer-service'>Pozycjonowanie</div>
+            <div className='footer-service'>Opieka nad stroną</div>
+            <div className='footer-service'>Kampanie Google Ads</div>
+            <div className='footer-service'>Systemy back-end</div>
+          </div>
+        </div>
+      </div>
+      <div className='footer-second-col'>
+        <div className='footer-links-title'>Przydatne linki</div>
+        <div className='footer-links'>
+          <div className='footer-link'>Nasze usługi</div>
+          <div className='footer-link'>Jak to działa</div>
+          <div className='footer-link'>Cennik</div>
+          <div className='footer-link'>Kontakt</div>
+          <div className='footer-link'>Polityka prywatności</div>
+          <div className='footer-link'>FAQ</div>
+
+          <div className='footer-link'>Zamów witrynę już teraz</div>
+        </div>
+      </div>
+      <div className='footer-third-col'></div>
+      <div className='footer-fourth-col'></div>
+
+      <div className='copy'>&copy;InteFuture 2024 - Wszelkie Prawa Zastrzeżone</div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -15,6 +15,7 @@ import { GoProjectSymlink } from 'react-icons/go';
 import { IoCloudUploadOutline } from 'react-icons/io5';
 import { IoMdCheckmarkCircle } from 'react-icons/io';
 import { HiQuestionMarkCircle } from 'react-icons/hi';
+import Footer from '../Components/Footer';
 const MainPage: React.FC = () => {
   return (
     <div className='container'>
@@ -319,40 +320,7 @@ const MainPage: React.FC = () => {
           </div>
         </div>
       </div>
-      <div className='footer'>
-        <div className='footer-first-col'>
-          <div className='footer-logo'>InteFuture</div>
-          <div className='slogan'>Przyciągniemy do Ciebie uwagę</div>
-          <div className='footer-services-root'>
-            <div className='footer-services-title'>Usługi</div>
-            <div className='footer-services'>
-              <div className='footer-service'>Tworzenie witryn</div>
-              <div className='footer-service'>Aktualizacja witryn</div>
-              <div className='footer-service'>Pozycjonowanie</div>
-              <div className='footer-service'>Opieka nad stroną</div>
-              <div className='footer-service'>Kampanie Google Ads</div>
-              <div className='footer-service'>Systemy back-end</div>
-            </div>
-          </div>
-        </div>
-        <div className='footer-second-col'>
-          <div className='footer-links-title'>Przydatne linki</div>
-          <div className='footer-links'>
-            <div className='footer-link'>Nasze usługi</div>
-            <div className='footer-link'>Jak to działa</div>
-            <div className='footer-link'>Cennik</div>
-            <div className='footer-link'>Kontakt</div>
-            <div className='footer-link'>Polityka prywatności</div>
-            <div className='footer-link'>FAQ</div>
-
-            <div className='footer-link'>Zamów witrynę już teraz</div>
-          </div>
-        </div>
-        <div className='footer-third-col'></div>
-        <div className='footer-fourth-col'></div>
-
-        <div className='copy'>&copy;InteFuture 2024 - Wszelkie Prawa Zastrzeżone</div>
-      </div>
+      <Footer />
     </div>
   );
 };
